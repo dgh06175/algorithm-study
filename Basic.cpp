f5010722c4e3884a5baa2235e7f61d59619cf273
dgh06175 <77305722+dgh06175@users.noreply.github.com>
2021-02-23 17:45:37 +0900

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -115,3 +115,108 @@ using namespace std;
 //	}
 //	cout << sum;
 //}
+
+/* 최대공약수와 최대공배수 */
+//int GCD(int a, int b)
+//{
+//	while (b!=0)
+//	{
+//		int r = a % b;
+//		a = b;
+//		b = r;
+//	}
+//	return a;
+//}
+//
+//int GCD1(int a, int b)
+//{
+//	if (b == 0)return a;
+//	else return GCD1(b, a % b);
+//}
+//
+//int main()
+//{
+//	int a, b, g;
+//	cin >> a >> b;
+//	g = GCD1(a, b);
+//	cout << g << '\n' << g * (a / g) * (b / g);
+//}
+
+/* 백준 1978 */
+//bool is_prime(int n)
+//{
+//	if (n < 2) return false;
+//	for (int i = 2; i * i <= n; i++)
+//		if (n % i == 0) return false;
+//	return true;
+//}
+//
+//int main()
+//{
+//	int n;
+//	int count = 0;
+//	cin >> n;
+//	int* x = new int[n];
+//	for (int i = 0; i < n; i++)
+//		cin >> x[i];
+//	for (int i = 0; i < n; i++)
+//		if (is_prime(x[i])) count++;
+//	cout << count;
+//}
+
+/* 1929 */
+//bool is_prime(int n)
+//{
+//	if (n < 2) return false;
+//	for (int i = 2; i * i <= n; i++)
+//		if (n % i == 0) return false;
+//	return true;
+//}
+//
+//int main()
+//{
+//	int n, m;
+//	int count = 0;
+//	cin >> n >> m;
+//	for (int i = n; i <= m; i++)
+//		if (is_prime(i)) cout << i << '\n';
+//}
+
+/* 1929 에라토머시기 체 */
+const int MAX = 100000;
+int main()
+{
+	bool check[MAX+1] = { 0, };
+	int a = MAX;
+	check[0] = check[1] = true;
+	for (int i = 2; i <= a; i++) {
+		if (check[i] == false) {
+			for (int j = i * i; j <= a; j += i) {
+				check[j] = true;
+			}
+		}
+	}
+	int n, m;
+	cin >> n >> m;
+	for (int i = n; i <= m; i++) {
+		if (check[i] == false) cout << i << '\n';
+	}
+}
+
+//const int MAX = 1000000;
+//int main()
+//{
+//	int prime[100];
+//	int pn = 0;
+//	bool check[101] = { 0, };
+//	int n = 100;
+//	for (int i = 2; i <= n; i++) {
+//		if (check[i] == false) {
+//			prime[pn++] = i;
+//			for (int j = i * i; j <= n; j += i) {
+//				check[j] = true;
+//			}
+//		}
+//	}
+//	cout << pn;
+//}
