c3f69d31d38d634ca2b014f9387b02ee27128731
이상현 <dgh06175@gmail.com>
2025-01-17 17:54:02 +0900

    [Bronze III] Title: 더하기 3, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/11023. 더하기 3/더하기 3.java
+++ 백준/Bronze/11023. 더하기 3/더하기 3.java
@@ -1,17 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static List<Integer> parseInputSplitToList(String splitRegex) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] tokens = br.readLine().split(splitRegex);
-        return Arrays.stream(tokens)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        String line = br.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(line, splitRegex);
+        List<Integer> numbers = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            numbers.add(Integer.parseInt(tokenizer.nextToken()));
+        }
+        br.close();
+        return numbers;
     }
 
     public static void main(String args[]) throws IOException {
