ad01d5d475342ee45c080e5c7e140a0826fdcd81
이상현 <dgh06175@gmail.com>
2025-01-28 17:34:12 +0900

    [level 2] Title: 가장 큰 수, Time: 237.79 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,31 +1,13 @@
 import java.util.*;
-import java.util.stream.*;
+import java.util.stream.Collectors;
 
 class Solution {
-    private int compare(Integer a, Integer b) {
-        String strA = String.valueOf(a);
-        String strB = Integer.toString(b);
-        int tmp1 = Integer.valueOf(strA + strB);
-        int tmp2 = Integer.valueOf(strB + strA);
-        return tmp1 - tmp2;
-    }
-    
-    public String solution(int[] numbers) {
-        Integer[] ary = Arrays.stream(numbers)
-            .mapToObj(Integer::valueOf)
-            .toArray(Integer[]::new);
-        String answer = "";
-        Arrays.sort(ary, (x1, x2) -> compare(x2, x1));
-        int zeroCount = 0;
-        for(var m: ary){
-            answer += m;
-            if (m == 0) {
-                zeroCount ++;
-            }
-        }
-        if (zeroCount == ary.length) {
+    public static String solution(int[] numbers) {
+        if (Arrays.stream(numbers).sum() == 0)
             return "0";
-        }
-        return answer;
+        return Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((a, b) -> (b + a).compareTo(a + b))
+                .collect(Collectors.joining(""));
     }
 }
