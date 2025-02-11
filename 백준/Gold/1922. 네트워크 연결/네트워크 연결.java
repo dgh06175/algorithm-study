94197ac0dfec0c4fbd1b17761ddd988863a8a5f4
이상현 <dgh06175@gmail.com>
2025-02-11 21:41:27 +0900

    [Gold IV] Title: 네트워크 연결, Time: 1176 ms, Memory: 242528 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -14,12 +14,52 @@ class Main
 		}
 	}
 	
+	static class UnionFind {
+		int[] parent;
+		int[] rank;
+		
+		UnionFind(int size) {
+			this.parent = new int[size];
+			for(int i = 0; i < size; i++) {
+				parent[i] = i;
+			}
+			this.rank = new int[size];
+		}
+		
+		int find(int x) {
+			if (parent[x] == x) {
+				return x;
+			}
+			return parent[x] = find(parent[x]);
+		}
+		
+		boolean union(int a, int b) {
+			int rootA = find(a);
+			int rootB = find(b);
+			
+			if (rootA == rootB) {
+	            return false;
+	        }
+	        
+	        if (rank[rootA] < rank[rootB]) {
+	            parent[rootA] = rootB;
+	        } else if (rank[rootA] > rank[rootB]) {
+	            parent[rootB] = rootA;
+	        } else {
+	            parent[rootB] = rootA;
+	            rank[rootA]++;
+	        }
+	        return true;
+		}
+	}
+	
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		
+		UnionFind uf = new UnionFind(N + 1);
 		List<Edge> edges = new ArrayList<>();
 		while(M-- > 0) {
 			int a = sc.nextInt();
@@ -28,49 +68,17 @@ class Main
 			edges.add(new Edge(a, b, c));
 		}
 		
-		int[] parent = new int[N + 1];
-		int[] rank = new int[N + 1];
-		for(int i = 0; i < N + 1; i++) {
-			parent[i] = i;
-		}
-		
 		edges.sort((e1, e2) -> Integer.compare(e1.cost, e2.cost));
 		int answer = 0;
 		int count = 0;
 		for(Edge edge: edges) {
-			if (find(parent, edge.from) != find(parent, edge.to)) {
-				union(parent, rank, edge.from, edge.to);
+			if (uf.union(edge.from, edge.to)) {
 				answer += edge.cost;
-				if (count++ == N - 1) {
+				if (++count == N - 1) {
 					break;
 				}
 			}
 		}
 		System.out.println(answer);
-	}
-	
-    static int find(int[] parent, int x) {
-		if (parent[x] == x) {
-			return x;
-		}
-		return parent[x] = find(parent, parent[x]);
-	}
-	
-	static void union(int[] parent, int[] rank, int a, int b) {
-		int rootA = find(parent, a);
-		int rootB = find(parent, b);
-		
-		if (rootA == rootB) {
-            return;
-        }
-        
-        if (rank[rootA] < rank[rootB]) {
-            parent[rootA] = rootB;
-        } else if (rank[rootA] > rank[rootB]) {
-            parent[rootB] = rootA;
-        } else {
-            parent[rootB] = rootA;
-            rank[rootA]++;
-        }
 	}
 }
