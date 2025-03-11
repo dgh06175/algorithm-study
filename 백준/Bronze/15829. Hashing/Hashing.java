2795afbd5bf05daa18341328484db474cd9b3e1c
이상현 <dgh06175@gmail.com>
2025-03-11 17:04:03 +0900

    [Bronze II] Title: Hashing, Time: 164 ms, Memory: 17700 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static final int R = 31;
+    private static final int M = 1_234_567_891;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int l = Integer.parseInt(sc.nextLine());
+        String str = sc.nextLine();
+
+        int hash = calcHash(str, l);
+        System.out.println(hash);
+    }
+
+    private static int calcHash(String str, int len) {
+        int hash = 0;
+        for (int i = 0; i < len; i++) {
+            int value = str.charAt(i) - 'a' + 1;
+            hash += (value * (int) Math.pow(R, i)) % M;
+        }
+        return hash % M;
+    }
+}
