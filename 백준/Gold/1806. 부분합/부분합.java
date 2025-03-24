faf362fdfa58a4733af0ccdce610c88df6721d3d
이상현 <dgh06175@gmail.com>
2025-03-24 13:58:18 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 23524 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -13,43 +13,28 @@ public class Main {
         n = Integer.parseInt(input[0]);
         s = Integer.parseInt(input[1]);
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        ary = new int[n];
+        ary = new int[n + 1];
         for (int i = 0; i < n; i++) {
             ary[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 이분탐색으로 길이: 1부터 n까지 탐색 O(logN)
-        int start = 1;
-        int end = n;
-        int ans = 0;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (isValid(mid)) {
-                ans = mid;
-                end = mid - 1;
+        int start = 0;
+        int end = 0;
+        int sum = ary[0];
+        int ans = Integer.MAX_VALUE;
+        while (start <= end && end < n) {
+            if (sum >= s) {
+                ans = Math.min(end - start + 1, ans);
+                sum -= ary[start++];
             } else {
-                start = mid + 1;
+                sum += ary[++end];
             }
+        }
+
+        if (ans == Integer.MAX_VALUE) {
+            System.out.println(0);
+            return;
         }
         System.out.println(ans);
-    }
-
-    // func: x의 길이로 합을 만들 수 있으면 true 반환 O(N)
-    private static boolean isValid(int x) {
-        int sum = 0;
-        for (int i = 0; i < x; i++) {
-            sum += ary[i];
-        }
-        for (int i = x; i < n; i++) {
-            if (sum >= s) {
-                return true;
-            }
-            sum += ary[i];
-            sum -= ary[i - x];
-        }
-        if (sum >= s) {
-            return true;
-        }
-        return false;
     }
 }
