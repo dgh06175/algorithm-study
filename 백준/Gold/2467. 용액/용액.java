746fbedac342ad3f0a0174f588776634aa87750d
이상현 <dgh06175@gmail.com>
2025-05-15 18:48:12 +0900

    [Gold V] Title: 용액, Time: 332 ms, Memory: 28840 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,59 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int[] ary = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long[] ary = new long[n];
         for (int i = 0; i < n; i++) {
-            ary[i] = Long.parseLong(st.nextToken());
+            ary[i] = Integer.parseInt(st.nextToken()); // -10억 ~ 10억
         }
-        long[] ans = new long[] { ary[0], ary[1] };
-        long minDiff = Math.abs(ary[0] + ary[1]);
+        Arrays.sort(ary);
+        long minSum = Long.MAX_VALUE;
+        int[] result = new int[] { Integer.MAX_VALUE, Integer.MAX_VALUE };
 
-        for (int i = 0; i < n; i++) {
-            long x = ary[i];
-            long y = find(ary, i);
-            long diff = Math.abs(x + y);
-            if (minDiff > diff) {
-                minDiff = diff;
-                ans[0] = x;
-                ans[1] = y;
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            long sum = (long) ary[left] + ary[right];
+
+            if (Math.abs(sum) < Math.abs(minSum)) {
+                minSum = sum;
+                result[0] = ary[left];
+                result[1] = ary[right];
             }
-        }
 
-        if (ans[0] > ans[1]) {
-            System.out.println(ans[1] + " " + ans[0]);
-        } else {
-            System.out.println(ans[0] + " " + ans[1]);
-        }
-    }
-
-    private static long find(long[] ary, int index) {
-        long x = ary[index];
-        int start = 0;
-        int end = ary.length - 1;
-        long minDiff = Long.MAX_VALUE;
-        long ans = Long.MIN_VALUE;
-
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            long value = ary[mid] + x;
-            long diff = Math.abs(value);
-            if (minDiff > diff && mid != index) {
-                minDiff = diff;
-                ans = ary[mid];
-            }
-            if (value < 0) {
-                start = mid + 1;
+            if (sum < 0) {
+                left++;
             } else {
-                end = mid - 1;
+                right--;
             }
         }
-        return ans;
+
+        for (int i = 0; i < 2; i++) {
+            System.out.print(result[i] + " ");
+        }
     }
 }
