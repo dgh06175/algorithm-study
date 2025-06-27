3321d6b6351e4082685c41bc797cc2e3e2d02a21
이상현 <dgh06175@gmail.com>
2025-06-27 21:26:47 +0900

    [Silver V] Title: 카드1, Time: 68 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+n = int(input())
+
+queue = deque([i for i in range(1, n + 1)])
+
+while len(queue) > 1:
+    x = queue.popleft()
+    print(x, end=' ')
+    queue.append(queue.popleft())
+
+if queue:
+    print(queue.pop())
