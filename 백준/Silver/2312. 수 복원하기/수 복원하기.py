fc76fc9c2b6626ef51fdcf00bf6206e93f4d6e0f
이상현 <dgh06175@gmail.com>
2025-07-10 22:35:44 +0900

    [Silver III] Title: 수 복원하기, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    if n == 1:
+        print("1 1")
+        continue
+    for i in range(2, int(n**0.5) + 1):
+        count = 0
+        while n % i == 0:
+            n //= i
+            count += 1
+        if count != 0:
+            print(f"{i} {count}")
+    if n > 1:
+        print(f"{n} 1")
