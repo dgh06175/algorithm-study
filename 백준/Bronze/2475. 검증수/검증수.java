8aafb4b3d5ca6ae9bcf83515bb7fcca5dca82ccb
이상현 <dgh06175@gmail.com>
2025-09-20 16:09:35 +0900

    [Bronze V] Title: 검증수, Time: 164 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        for(int i = 0; i < 5; i++) {
+            int num = sc.nextInt();
+            ans += (num * num) % 10;
+        }
+        System.out.println(ans % 10);
+    }
+}
