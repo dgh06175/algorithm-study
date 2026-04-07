055f68c7ef0ae086b992f87efd69326d09bd354b
이상현 <dgh06175@gmail.com>
2026-04-07 19:23:39 +0900

    [Silver II] Title: 유기농 배추, Time: 152 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,66 +2,70 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dy = { 1, -1, 0, 0 };
-    static int[] dx = { 0, 0, 1, -1 };
+    static final int[] dy = {1, -1, 0, 0};
+    static final int[] dx = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
         while (t-- > 0) {
             st = new StringTokenizer(br.readLine());
             int m = Integer.parseInt(st.nextToken());
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
 
-            boolean board[][] = new boolean[n][m];
+            boolean[][] board = new boolean[n][m];
+            List<int[]> cabbages = new ArrayList<>(k);
+
             for (int i = 0; i < k; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
                 board[y][x] = true;
+                cabbages.add(new int[]{y, x});
             }
 
-            int answer = bfs(board, n, m);
-            System.out.println(answer);
+            sb.append(countComponents(board, cabbages, n, m)).append('\n');
         }
+
+        System.out.print(sb);
     }
 
-    static int bfs(boolean[][] board, int n, int m) {
+    static int countComponents(boolean[][] board, List<int[]> cabbages, int n, int m) {
         int answer = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
 
-        boolean[][] visited = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!board[i][j] || visited[i][j]) {
-                    continue;
-                }
-                answer++;
+        for (int[] pos : cabbages) {
+            int sy = pos[0];
+            int sx = pos[1];
 
-                Queue<int[]> queue = new ArrayDeque<>();
-                queue.offer(new int[] { i, j });
-                visited[i][j] = true;
+            if (!board[sy][sx]) continue;
 
-                while (!queue.isEmpty()) {
-                    int[] now = queue.poll();
-                    int y = now[0];
-                    int x = now[1];
+            answer++;
+            board[sy][sx] = false;
+            queue.offer(sy * m + sx);
 
-                    for (int d = 0; d < 4; d++) {
-                        int ny = y + dy[d];
-                        int nx = x + dx[d];
-                        if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
-                            continue;
-                        }
-                        if (board[ny][nx] && !visited[ny][nx]) {
-                            queue.offer(new int[] { ny, nx });
-                            visited[ny][nx] = true;
-                        }
-                    }
+            while (!queue.isEmpty()) {
+                int cur = queue.poll();
+                int y = cur / m;
+                int x = cur % m;
+
+                for (int d = 0; d < 4; d++) {
+                    int ny = y + dy[d];
+                    int nx = x + dx[d];
+
+                    if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                    if (!board[ny][nx]) continue;
+
+                    board[ny][nx] = false;
+                    queue.offer(ny * m + nx);
                 }
             }
         }
+
         return answer;
     }
 }
