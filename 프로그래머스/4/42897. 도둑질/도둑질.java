4c749692fd1fcf8a2e167e6797d2a60ff93c950c
이상현 <dgh06175@gmail.com>
2026-05-16 22:19:58 +0900

    [level 4] Title: 도둑질, Time: 15.46 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,31 +1,31 @@
 class Solution {
     public int solution(int[] money) {
         int n = money.length;
-        int[][] dp = new int[n][2];
+        int[] dp = new int[n];
         return Math.max(rob1(money, n, dp), rob2(money, n, dp));
     }
     
-    int rob1(int[] money, int n, int[][] dp) { // 첫번쨰 방 털기
-        dp[0][0] = money[0];
-        dp[0][1] = 0;
+    int rob1(int[] money, int n, int[] dp) { // 첫번쨰 방 털기
+        dp[0] = money[0];
+        dp[1] = Math.max(money[0], money[1]);
         
-        for(int i = 1; i < n - 1; i++) {
-            dp[i][0] = dp[i-1][1] + money[i];
-            dp[i][1] = Math.max(dp[i-1][0], dp[i-1][1]);
+        for(int i = 2; i < n - 1; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + money[i]);
+            // dp[i][0] = dp[i-1][1] + money[i];
+            // dp[i][1] = Math.max(dp[i-1][0], dp[i-1][1]);
         }
         
-        return Math.max(dp[n-2][0], dp[n-2][1]);
+        return dp[n - 2];
     }
     
-    int rob2(int[] money, int n, int[][] dp) { // 첫번쨰 방 안털기
-        dp[0][0] = 0;
-        dp[0][1] = 0;
+    int rob2(int[] money, int n, int[] dp) { // 첫번쨰 방 안털기
+        dp[0] = 0;
+        dp[1] = money[1];
         
-        for(int i = 1; i < n; i++) {
-            dp[i][0] = dp[i-1][1] + money[i];
-            dp[i][1] = Math.max(dp[i-1][0], dp[i-1][1]);
+        for(int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + money[i]);
         }
         
-        return Math.max(dp[n-1][0], dp[n-1][1]);
+        return dp[n - 1];
     }
 }
