d1aac94b3dad317acc9f5f65b11921b931807f85
이상현 <dgh06175@gmail.com>
2026-05-16 16:09:41 +0900

    [level 3] Title: 양과 늑대, Time: 2.45 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    int ans = 0;
+    int n = 0;
+    
+    public int solution(int[] info, int[][] edges) {
+        n = info.length;
+        List<Integer>[] graph = new ArrayList[n];
+        for(int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int i = 0; i < edges.length; i++) {
+            graph[edges[i][0]].add(edges[i][1]);
+            // graph[edges[i][1]].add(edges[i][0]);
+        }
+        dfs(info, graph, 0, 0, 0, new boolean[n]);
+        
+        return ans;
+    }
+    
+    void dfs(int[] info, List<Integer>[] graph, int node, int sheep_count, int wolf_count, boolean[] next_visit) {
+        if (info[node] == 0) {
+            sheep_count += 1;
+        } else {
+            wolf_count += 1;
+        }
+        
+        if (sheep_count <= wolf_count) {
+            return;
+        }
+        ans = Math.max(ans, sheep_count);
+        
+        next_visit[node] = false;
+        
+        for(int next: graph[node]) {
+            next_visit[next] = true;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if (next_visit[i]) {
+                dfs(info, graph, i, sheep_count, wolf_count, next_visit.clone());
+            }
+        }
+    }
+}
